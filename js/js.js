5e84c6d9c99648a022582fce5dc81b86d1315fb4
laaluuzz <laluznicolas@gmail.com>
2024-07-17 21:21:45 -0400

    a

--- js/js.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    var audio = document.getElementById('miAudio');
-    
-    // Reproducir automáticamente el audio al hacer clic en cualquier parte de la página
-    $(document).click(function() {
-        audio.play();
-    });
-});
